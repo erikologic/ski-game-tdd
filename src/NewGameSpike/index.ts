fdf604d0f21b017e8cc1b203c62ee3ccd9a931bd
Enrico Graziani <14198729+erikologic@users.noreply.github.com>
2024-11-13 12:24:53 +0000

    player moves between states

--- src/NewGameSpike/index.ts
+++ src/NewGameSpike/index.ts
@@ -13,57 +13,85 @@ export interface IEntity {
     next(time: number): void;
 }
 
-export class Player implements IEntity {
-    speed = 0.02;
-    lastTime = 0;
-    position = new Position(0, 0);
-    _state: "downhill" | "jumping" = "downhill";
-    assetManager: AssetManager;
+interface IEntityState {
+    speed: number;
+    nextState(time: number): IEntityState;
+    animation: Animation;
+    do(command: "jump"): IEntityState;
+}
+
+class JumpingState implements IEntityState {
+    speed = 0.015;
     animation: Animation;
 
-    constructor(assetManager: AssetManager) {
-        this.assetManager = assetManager;
+    constructor(private assetManager: AssetManager) {
+        this.animation = new Animation(
+            [
+                assetManager.images["img/skier_jump_1.png"],
+                assetManager.images["img/skier_jump_2.png"],
+                assetManager.images["img/skier_jump_3.png"],
+                assetManager.images["img/skier_jump_4.png"],
+                assetManager.images["img/skier_jump_5.png"],
+            ],
+            false
+        );
+    }
+
+    nextState(time: number) {
+        if (this.animation.complete) {
+            return new DownhillState(this.assetManager);
+        }
+        this.animation.update(time);
+        return this;
+    }
+
+    do(action: "jump"): IEntityState {
+        return this;
+    }
+}
+
+class DownhillState implements IEntityState {
+    speed = 0.03;
+    animation: Animation;
+
+    constructor(private assetManager: AssetManager) {
         this.animation = new Animation([assetManager.images["img/skier_down.png"]]);
     }
 
-    set state(value: "downhill" | "jumping") {
-        if (value === this._state) {
-            return;
-        }
-        this._state = value;
-        if (value === "jumping") {
-            this.animation = new Animation(
-                [
-                    this.assetManager.images["img/skier_jump_1.png"],
-                    this.assetManager.images["img/skier_jump_2.png"],
-                    this.assetManager.images["img/skier_jump_3.png"],
-                    this.assetManager.images["img/skier_jump_4.png"],
-                    this.assetManager.images["img/skier_jump_5.png"],
-                ],
-                false
-            );
-            this.speed *= 0.5;
-        }
-        if (value === "downhill") {
-            this.animation = new Animation([this.assetManager.images["img/skier_down.png"]]);
-            this.speed *= 2;
-        }
+    nextState(time: number) {
+        return this;
+    }
+
+    do(command: "jump"): IEntityState {
+        return new JumpingState(this.assetManager);
+    }
+}
+
+export class Player implements IEntity {
+    lastTime = 0;
+    position = new Position(0, 0);
+    assetManager: AssetManager;
+    state: IEntityState;
+
+    constructor(assetManager: AssetManager) {
+        this.assetManager = assetManager;
+        this.state = new DownhillState(assetManager);
+    }
+
+    do(action: "jump") {
+        this.state = this.state.do(action);
     }
 
     next(time: number) {
         const timeDiff = time - this.lastTime;
         this.lastTime = time;
 
-        if (this._state === "jumping" && this.animation.complete) {
-            this.state = "downhill";
-        } else {
-            this.animation.update(time);
-        }
-        this.position.y += this.speed * timeDiff;
+        this.state = this.state.nextState(time);
+        this.position.y += this.state.speed * timeDiff;
     }
 
     get frame() {
-        return this.animation.frame;
+        return this.state.animation.frame;
     }
 }
 
@@ -120,7 +148,7 @@ document.addEventListener("DOMContentLoaded", async () => {
 
     async function next(time: number) {
         if (time > 2000 && time < 2050) {
-            player.state = "jumping";
+            player.do("jump");
         }
 
         entities.forEach((entity) => entity.next(time));
